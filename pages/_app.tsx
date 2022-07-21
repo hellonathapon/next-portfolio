33238d301d1093ff4241bebb9ef3fd279539aa87
nathapon <hellonathapon@gmail.com>
2022-07-21 10:20:03 +0700

    Adjust max width to readability blog post style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { Header } from "../components";
+import { Header, Footer } from "../components";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Header />
       <Component {...pageProps} />
+      <Footer />
     </>
   );
 }
